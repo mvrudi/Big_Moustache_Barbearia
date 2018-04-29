8da5b0ca54c38a3e21e234db5d66845fcb9b08b2
mvrudi <marcusreisudi@gmail.com>
2018-04-28 21:13:10 -0300

    New funtionality added to foto panel using jQuery and Javascript

--- _script/script.js
+++ _script/script.js
@@ -2,12 +2,44 @@
 var arrayFotos = ['_imagens/foto1.jpg','_imagens/foto2.jpg','_imagens/foto3.jpg','_imagens/foto4.jpg','_imagens/foto5.jpg'];
 var foto = 0;
 var painelFoto = document.querySelector(".fotoPainel");
+var borderImg="";
 
 function trocaFoto () {
     painelFoto.src=arrayFotos[foto];
+    trocaPainel(foto);
     foto += 1;
     if (foto == 5) {
         foto = 0;
     }
+    
 }
+
 setInterval(trocaFoto, 3000);
+//setInterval(trocaPainel, 3000);
+
+function trocaPainel(foto){
+    
+    switch (foto) {
+        case 0:
+            $("#foto.foto5").css("opacity",'.2');
+            $("#foto.foto1").css("opacity",'1');
+            break;
+        case 1:
+            $("#foto.foto1").css("opacity",'.2');
+            $("#foto.foto2").css("opacity",'1');
+            break;
+        case 2:
+            $("#foto.foto2").css("opacity",'.2');            
+            $("#foto.foto3").css("opacity",'1');
+            break;
+        case 3:
+            $("#foto.foto3").css("opacity",'.2');
+            $("#foto.foto4").css("opacity",'1');
+            break;
+        case 4:
+            $("#foto.foto4").css("opacity",'.2');
+            $("#foto.foto5").css("opacity",'1');
+    }
+
+}
+
